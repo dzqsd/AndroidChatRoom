2916686e32547b0588f008548dd93732b45bc0ba
gjy <1418545922@qq.com>
2024-04-05 14:44:20 +0800

    欢迎界面添加滚动轮播

--- app/src/main/java/com/gjy/chatroom2/activities/WelcomeActivity.java
+++ app/src/main/java/com/gjy/chatroom2/activities/WelcomeActivity.java
@@ -1,16 +1,31 @@
 package com.gjy.chatroom2.activities;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.gjy.chatroom2.R;
+import com.gjy.chatroom2.javabean.BannerDataInfo;
+import com.youth.banner.Banner;
+import com.youth.banner.adapter.BannerImageAdapter;
+import com.youth.banner.holder.BannerImageHolder;
+import com.youth.banner.indicator.CircleIndicator;
+import com.youth.banner.listener.OnBannerListener;
+import org.checkerframework.checker.units.qual.C;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class WelcomeActivity extends AppCompatActivity {
     private TextView tv_countdown;
     private CountDownTimer countDownTimer;
-    private long timeLeftInMillis = 6000;//倒计时时长 5000毫秒
+    private long timeLeftInMillis = 30000;//倒计时时长 30000毫秒 30秒
+    private Banner banner;
+    private List<BannerDataInfo> mBannerDataInfos = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +34,39 @@ public class WelcomeActivity extends AppCompatActivity {
 
         //初始化控件
         tv_countdown = findViewById(R.id.tv_countdown);
+        banner = findViewById(R.id.banner);
+
+        //图片轮播功能
+        mBannerDataInfos.add(new BannerDataInfo(R.drawable.cc_face, "我是cc1"));
+        mBannerDataInfos.add(new BannerDataInfo(R.drawable.cc_face2, "我是cc2"));
+        mBannerDataInfos.add(new BannerDataInfo(R.drawable.cc_face3, "我是cc3"));
+
+        //设置轮播间隔时间
+        banner.setLoopTime(3000);//3000毫秒切换一次图片
+        //设置adapter
+        banner.setAdapter(new BannerImageAdapter<BannerDataInfo>(mBannerDataInfos) {
+                    @Override
+                    public void onBindView(BannerImageHolder bannerImageHolder, BannerDataInfo bannerDataInfo, int i, int i1) {
+                        //设置图片
+                        bannerImageHolder.imageView.setImageResource(bannerDataInfo.getImg());
+                    }
+
+                })
+                .addBannerLifecycleObserver(this)
+                .setIndicator(new CircleIndicator(this));
+
+        //画廊效果
+        //banner.setBannerGalleryEffect(10,10);
+
+        //魅族效果
+        banner.setBannerGalleryMZ(10);
+        //banner设置点击事件
+        banner.setOnBannerListener(new OnBannerListener<BannerDataInfo>() {
+            @Override
+            public void OnBannerClick(BannerDataInfo bannerDataInfo, int i) {
+                Toast.makeText(WelcomeActivity.this, bannerDataInfo.getTitle(), Toast.LENGTH_SHORT).show();
+            }
+        });
 
         //启动倒计时
         startCountDown();
---
+++ app/src/main/java/com/gjy/chatroom2/javabean/BannerDataInfo.java
@@ -0,0 +1,27 @@
+package com.gjy.chatroom2.javabean;
+
+public class BannerDataInfo {
+    private int img;
+    private String title;
+
+    public int getImg() {
+        return img;
+    }
+
+    public void setImg(int img) {
+        this.img = img;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public BannerDataInfo(int img, String title) {
+        this.img = img;
+        this.title = title;
+    }
+}
